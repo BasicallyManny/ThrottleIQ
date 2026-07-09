feb61e5b6360b08224e5a31acfae901d808803e4
Manfred Fong <mafong081102@gmail.com>
2026-07-09 14:19:05 -0400

    created vite project and installed tailwind as styler

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl text-red-200 w-full justify-center items-center">
+        ThrottleIQ
+      </div>
+    </>
+  )
+}
+
+export default App
